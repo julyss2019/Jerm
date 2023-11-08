4dc092c522bcb68020fd69fbdad1017ddf3b6f52
void <july_ss_2017@outlook.com>
2023-11-08 21:22:25 +0800

    fix: 修正 clone 返回类型错误

--- core/src/main/java/com/void01/bukkit/jerm/core/gui/component/TextureImpl.kt
+++ core/src/main/java/com/void01/bukkit/jerm/core/gui/component/TextureImpl.kt
@@ -2,7 +2,6 @@ package com.void01.bukkit.jerm.core.gui.component
 
 import com.germ.germplugin.api.dynamic.gui.GermGuiTexture
 import com.void01.bukkit.jerm.api.common.gui.Gui
-import com.void01.bukkit.jerm.api.common.gui.component.Component
 import com.void01.bukkit.jerm.api.common.gui.component.Texture
 import com.void01.bukkit.jerm.core.util.GermUtils
 
--- core/src/main/java/com/void01/bukkit/jerm/core/gui/component/UnsupportedComponent.kt
+++ core/src/main/java/com/void01/bukkit/jerm/core/gui/component/UnsupportedComponent.kt
@@ -2,7 +2,6 @@ package com.void01.bukkit.jerm.core.gui.component
 
 import com.germ.germplugin.api.dynamic.gui.GermGuiPart
 import com.void01.bukkit.jerm.api.common.gui.Gui
-import com.void01.bukkit.jerm.api.common.gui.component.Component
 import com.void01.bukkit.jerm.core.util.GermUtils
 
 class UnsupportedComponent(gui: Gui, handle: GermGuiPart<*>) :
